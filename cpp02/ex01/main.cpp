6c1f69d687a1b2fb43badc9bc89ec90b350b0b0e
charmstr <charmstr@mini-de-test.home>
2020-12-12 08:54:28 +0100

    modified the main for the corrector

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,13 +6,14 @@
 /*   By: charmstr <charmstr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 06:46:28 by charmstr          #+#    #+#             */
-/*   Updated: 2020/12/12 08:39:50 by charmstr         ###   ########.fr       */
+/*   Updated: 2020/12/12 08:53:41 by charmstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
+/*
 int main(void)
 {
 	Fixed b(255);
@@ -28,5 +29,22 @@ int main(void)
 	std::cout << f.toInt() << std::endl;
 	std::cout << f.toFloat() << std::endl;
 	std::cout << f << std::endl;
+	return 0;
+}
+*/
+int main(void) {
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+	a = Fixed(1234.4321f);
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
